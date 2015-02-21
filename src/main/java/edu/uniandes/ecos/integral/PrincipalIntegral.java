003a6b9705cf68de7b741bbb41095e5878e1b72a
Javier Virviescas <virtoja13@hotmail.com>
2015-02-20 20:37:55 -0500

    Primera version del proyecto Program5 Javier Virviescas

---
+++ src/main/java/edu/uniandes/ecos/integral/PrincipalIntegral.java
@@ -0,0 +1,13 @@
+package edu.uniandes.ecos.integral;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrincipalIntegral 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
